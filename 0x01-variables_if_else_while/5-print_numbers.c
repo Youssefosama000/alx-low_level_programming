fb76399ea468d33773747709822ae64980e5a0c0
Youssef osama <youssefosama380@gmail.com>
2023-07-14 11:35:17 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: a c program
+ *
+ * Return: 0
+**/
+
+int main(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		printf("%d", x);
+		x++;
+	}
+	printf("\n");
+
+	return (0);
+}
