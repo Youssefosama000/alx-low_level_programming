352f1be1966eac9e6f698ebbabdb3afb792cb0e9
Youssef osama <youssefosama380@gmail.com>
2023-07-18 11:44:09 -0700

    d

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - get the last digit of a number
+ *
+ * @x: the number to be treated
+ *
+ * Return: value of the last digit number
+ */
+
+int print_last_digit(int x)
+{
+	int y;
+	y = x % 10;
+	if (y < 0)
+	{
+		y = y * -1;
+	}
+	_putchar(y + '0');
+	return (y);
+}
