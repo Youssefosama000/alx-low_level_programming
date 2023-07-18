8898e0e123c129a814625f4555fc3cd0ffeb2b02
Youssef osama <youssefosama380@gmail.com>
2023-07-18 11:24:16 -0700

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the number
+ *
+ * @n: the number to be checked
+ *
+ * Return: 1 for positive, -1 for negative and 0 for anything else
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
+}
