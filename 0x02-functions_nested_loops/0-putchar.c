4c6d6847fd1fd832753953f643be74a5808e88c5
Youssef osama <youssefosama380@gmail.com>
2023-07-18 09:31:34 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,11 +10,11 @@
 
 int main(void)
 {
-	str[] = "_putchar";
+	char str[] = "_putchar";
 	int i;
 
 	for (i = 0 ; i <= 8 ; i++)
 		_putchar(str[i]);
-	_putchar('/n');
+	_putchar('\n');
 	return (0);
 }
