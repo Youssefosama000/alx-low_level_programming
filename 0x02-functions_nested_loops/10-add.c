8229afd25a872c927ba98d80f009f5ba411257a2
Youssef osama <youssefosama380@gmail.com>
2023-07-18 12:06:38 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @x: first number
+ *
+ * @y: second number
+ *
+ * Return: integer number
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
