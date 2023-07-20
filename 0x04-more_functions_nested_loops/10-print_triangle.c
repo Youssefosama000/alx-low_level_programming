b1f6cf53940a4a454f3a6d783b47140a55736b5f
Youssef osama <youssefosama380@gmail.com>
2023-07-20 08:21:39 -0700

    a

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,22 +4,27 @@
  * print_triangle - print a triangle
  *
  * @size: the size of triangle
+ *
+ * Return: always 0 (Success)
 **/
 
 void print_triangle(int size)
 {
-	int x, y, z;
+	int x, y;
 
 	if (size <= 0)
 		_putchar('\n');
 	else
 	{
-		for (x = 0 ; x < size ; x++)
+		for (x = 1 ; x <= size ; x++)
 		{
-			for (y = size - x ; y > 1 ; y--)
-				_putchar('#');
-			for (z = 0 ; z <= x ; z++)
-				_putchar(35);
+			for (y = 1 ; y <= size ; y++)
+			{
+				if ((x + y) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
 			_putchar('\n');
 		}
 	}
