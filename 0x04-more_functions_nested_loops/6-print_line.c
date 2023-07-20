e1407919e2095a8277164a5a70c3253393818cec
Youssef osama <youssefosama380@gmail.com>
2023-07-20 07:18:21 -0700

    a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: the number of times
+ *
+**/
+
+void print_line(int n)
+{
+	int x;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 1 ; x < n ; x++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
