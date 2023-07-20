2743d3489fd60cf9eeefe4ae9ab116a6244c90d8
Youssef osama <youssefosama380@gmail.com>
2023-07-20 07:25:48 -0700

    a

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ *
+ * @n: the number of times the / character
+ * should be printed
+**/
+
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (x = 1 ; x <= n ; x++)
+		{
+			for (y = 1 ; y <= x ; y++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
