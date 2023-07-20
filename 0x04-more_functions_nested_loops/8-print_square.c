56c703c6edde8344668aef6efaf324ca0259783c
Youssef osama <youssefosama380@gmail.com>
2023-07-20 07:47:29 -0700

    a

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,15 @@ void print_square(int size)
 {
 	int x, y;
 
-	for (x = 1 ; x <= size ; x++)
-	{
-		for (y = 1 ; y <= size ; y++)
-			_putchar('#');
+	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		for (x = 1 ; x <= size ; x++)
+		{
+			for (y = 1 ; y <= size ; y++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
