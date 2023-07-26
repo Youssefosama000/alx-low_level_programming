59449eb854375c4e1f67aacf3eb657127d5e6aa8
Youssef osama <youssefosama380@gmail.com>
2023-07-26 14:02:30 -0700

    a

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - to reverse an array
+ *
+ * @a: parameter 1
+ * @n: parameter 2
+ *
+ * Return: void
+**/
+
+void reverse_array(int *a, int n)
+{
+	int i, j, t;
+
+	for (i = 0 ; j = (n - 1) ; i < j ; i++ ; j--)
+	{
+		t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+	}
+}
