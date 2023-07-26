018f2104ae642aa5b5b269bc3938478f4872022c
Youssef osama <youssefosama380@gmail.com>
2023-07-26 13:55:20 -0700

    a

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - a c program
+ *
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ *
+ * Return: dest
+**/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
