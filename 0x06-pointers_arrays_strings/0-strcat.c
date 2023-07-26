196689479c6bba5272dc33ade6d137f1e6315898
Youssef osama <youssefosama380@gmail.com>
2023-07-26 13:45:34 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,14 @@
 
 /**
  * _strcat - a c program
+ *
  * @dest: a parameter
  * @src: a parameter
- * @n: a parameter
  *
  * Return: dest
 **/
 
-char *_strcat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int c, i;
 
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src, int n)
 
 	while (dest[c])
 		c++;
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; src[i]; i++)
 		dest[c++] = src[i];
 
 	dest[c++] = '\0';
