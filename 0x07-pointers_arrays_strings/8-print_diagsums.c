428294ebcaab8dc9b466ffa89ca5c4513312c5cd
Youssef osama <youssefosama380@gmail.com>
2023-07-31 16:37:11 -0700

    a

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,19 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - entry point
- * @a: parameter 1
- * @size: parameter 2
+ * _print_diagsums - entry point
+ * @a: parameter 1 (square matrix represented as a 1D array)
+ * @size: parameter 2 (size of the square matrix)
  * Return: nothing
 **/
 
-void print_diagsums(int *a, int size)
+void _print_diagsums(int *a, int size)
 {
-	int i, n, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-	for (i = 0; i <= (size * size); i = i + size - 1)
+	for (i = 0; i < size * size; i += size + 1)
 		sum1 += a[i];
-	for (n = size - 1 ; n <= (size * size) ; n = n + size - 1)
-		sum2 += a[n];
+
+	for (i = size - 1; i < size * size - 1; i += size - 1)
+		sum2 += a[i];
+
 	printf("%d, %d\n", sum1, sum2);
 }
+
