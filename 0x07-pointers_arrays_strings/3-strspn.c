4272d103e0e1fe16d330d08f8a7b31377aeb0a7b
Youssef osama <youssefosama380@gmail.com>
2023-07-31 16:11:22 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 
 /**
  * _strspn - entry point
- * @s:parameter 1
+ * @s: parameter 1
  * @accept: parameter 2
  * Return: integer n
 **/
@@ -13,17 +13,23 @@ unsigned int _strspn(char *s, char *accept)
 
 	value = 0;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		check = 0;
+	check = 0;
 
-		for (n = 0 ; accept[n] != '\0' ; n++)
+	for (n = 0; accept[n] != '\0'; n++)
+	{
+		if (accept[n] == s[i])
 		{
-			if (accept[n] == s[i])
-			{
-				value++;
-				check = 1;
-			}
+			value++;
+			check = 1;
+			break;
 		}
 	}
+
+	if (check == 0)
+		break;
+	}
+
+	return (value);
 }
